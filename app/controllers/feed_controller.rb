6aab7060d4ec14573be809b20ebdf0caa4d9a6fb
Niklas Hofer <niklas+dev@lanpartei.de>
2006-09-12 00:23:44 +0000

    add feeds, add, edit and remove filters
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@85 0623352e-500e-0410-8e18-d950026d1da6

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,9 +1,58 @@
 class FeedController < ApplicationController
   layout 'torrents'
+  in_place_edit_for :filter, :expression
+  in_place_edit_for :feed, :url
+  in_place_edit_for :feed, :title
   def index
     @feeds = Feed.find(:all, :order => 'created_at desc')
   end
 
   def new
+    @feed = Feed.new
   end
+
+  def check
+    url = params[:url]
+    if Feed.avaiable(url)
+      render :update do |page|
+        page[:feed_url].class = 'found'
+        page[:add_button].disabled = false
+        page[:add_button].value = 'add'
+      end
+    else
+      render :update do |page|
+        page[:feed_url].class = 'not_found'
+        page[:add_button].disabled = true
+        page[:add_button].value = 'not found'
+      end
+    end
+  end
+
+  def create
+    @feed = Feed.new(params[:feed])
+    if @feed.save
+      render :update do | page|
+        page[:feed_form].remove
+        page.insert_html :top, :feed_list, render(:partial => 'feed_list_item', :object => @feed )
+      end
+    else
+      render :update do |page|
+        page[:feed_form].update render(:partial => 'form')
+      end
+    end
+  end
+
+  def show
+    @feed = Feed.find params[:id], :include => 'filters'
+  end
+
+  def new_filter
+    @filter = Filter.create :expression => 'Click to Edit', :feed_id => params[:feed]
+  end
+
+  def delete_filter
+    @filter = Filter.find params[:id]
+    @filter.destroy
+  end
+
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,4 +3,20 @@ class Feed < ActiveRecord::Base
   validates_presence_of :url, :message => "URL is needed"
   validates_format_of :url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
   validates_presence_of :title, :message => "please give a title"
+
+  before_validation :set_url_as_title
+
+  def self.avaiable(url)
+    # very poor check if the page is avaiable
+    url =~ /oo/
+  end
+
+  def items
+    %w(Foo Bar Baz)
+  end
+
+  private
+  def set_url_as_title
+    self.title = self.url if !self.title or self.title.empty?
+  end
 end
