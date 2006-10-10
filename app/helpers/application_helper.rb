146cf020056de49d06096750ecc23720c444eb53
Niklas Hofer <niklas+dev@lanpartei.de>
2006-10-10 21:38:51 +0000

    group_link with auto "active"
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@104 0623352e-500e-0410-8e18-d950026d1da6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def group_link(action)
+    content_tag('li',
+                link_to(action.capitalize, :action => action, :controller => 'torrents'),
+                { :class => (@controller.action_name == action ? 'active' : '')}
+    )
+  end
 end
