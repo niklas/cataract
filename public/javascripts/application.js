85dd825b12ecafdee7ac07cf0e2a384bf84e4d8d
Niklas Hofer <niklas+dev@lanpartei.de>
2006-10-10 21:40:17 +0000

    menu hover animation stuff
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@108 0623352e-500e-0410-8e18-d950026d1da6

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var show_menu = false;
+var menu_ani = false;
+var observer = false;
+function MenuHovered() {
+   $('menustatus').innerHTML = 'hovered';
+   clearTimeout(observer);
+   if (!show_menu) {
+      show_menu = true;
+      menu_ani = new Effect.SlideDown('actions');
+   }
+   $('menustatus').innerHTML = 'opened';
+}
+function MenuUnhovered() {
+   $('menustatus').innerHTML = 'unhovered';
+   observer = setTimeout(function() {
+      $('menustatus').innerHTML = 'closing';
+      show_menu = false;
+      menu_ani = new Effect.SlideUp('actions');
+      $('menustatus').innerHTML = 'closed';
+   }, 1000);
+}
+
