009279948bd4fd8a4b4a76341e63e0e4abf63f60
Niklas Hofer <niklas+dev@lanpartei.de>
2006-11-23 20:50:01 +0000

    watch torrents only once
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@176 0623352e-500e-0410-8e18-d950026d1da6

--- app/models/watching.rb
+++ app/models/watching.rb
@@ -1,4 +1,5 @@
 class Watching < ActiveRecord::Base
   belongs_to :torrent
   belongs_to :user
+  validates_uniqueness_of :user, :scope => :torrent_id, :message => 'you can watch a torrent only once, else you would see double'
 end
