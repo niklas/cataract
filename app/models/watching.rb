c6a09d5120e7d02ee885d9c2a5753c6cad75a539
Niklas Hofer <niklas+dev@lanpartei.de>
2006-11-24 01:35:59 +0000

    correct validation for watching
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@183 0623352e-500e-0410-8e18-d950026d1da6

--- app/models/watching.rb
+++ app/models/watching.rb
@@ -1,5 +1,5 @@
 class Watching < ActiveRecord::Base
   belongs_to :torrent
   belongs_to :user
-  validates_uniqueness_of :user, :scope => :torrent_id, :message => 'you can watch a torrent only once, else you would see double'
+  validates_uniqueness_of :user_id, :scope => :torrent_id, :message => 'you can watch a torrent only once, else you would see double'
 end
