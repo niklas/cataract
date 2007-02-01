2d7330ad17a1a2d5bf123edca6192309afe84fc5
Niklas Hofer <niklas+dev@lanpartei.de>
2007-02-01 17:44:09 +0000

    move debian directory to branches
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@220 0623352e-500e-0410-8e18-d950026d1da6

--- bin/cataract-trayicon-0.1/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the cataract-trayicon package
-#
-0 4	* * *	root	cataract-trayicon_maintenance
--- bin/cataract-trayicon-0.1/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(cataract-trayicon):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="cataract-trayicon" command="/usr/bin/cataract-trayicon"
