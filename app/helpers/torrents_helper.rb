e9112a621ffbacbefc0af504828205d5e42f3b12
Niklas Hofer <niklas+dev@lanpartei.de>
2007-06-21 00:51:38 +0000

    short_error_messages_for
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@268 0623352e-500e-0410-8e18-d950026d1da6

--- app/helpers/torrents_helper.rb
+++ app/helpers/torrents_helper.rb
@@ -93,7 +93,17 @@ module TorrentsHelper
   def nice_error_messages_for(object_name, options = {})
     options = options.symbolize_keys
     object = instance_variable_get("@#{object_name}")
-    "Bad #{object_name.to_s.gsub("_", " ")}: " +
+    "Bad #{object.class.to_s.humanize}: " +
+    if object && !object.errors.empty?
+      object.errors.full_messages.join(', ')
+    else
+      ""
+    end
+  end
+
+  def short_error_messages_for(object_name, options = {})
+    options = options.symbolize_keys
+    object = instance_variable_get("@#{object_name}")
     if object && !object.errors.empty?
       object.errors.full_messages.join(', ')
     else
