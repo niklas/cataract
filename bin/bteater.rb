1358b4849964b0c009d40c9e3b0e99c12f1278e4
Niklas Hofer <niklas+dev@lanpartei.de>
2007-07-31 21:53:01 +0000

    we dont need the manual eater anymore
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@310 0623352e-500e-0410-8e18-d950026d1da6

--- bin/bteater.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-RAILS_ENV = ENV['RAILS_ENV'] || 'production'
-require File.dirname(__FILE__) + '/../config/environment'
-
-eater = BittornadoEater.new(ARGV.shift || 'flupp')
-eater.startup
-
