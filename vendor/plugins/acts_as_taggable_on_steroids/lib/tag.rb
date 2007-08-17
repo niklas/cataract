f5bb5505e62fa540c82a08d88a62e063dd1bfe50
Niklas Hofer <niklas+dev@lanpartei.de>
2007-08-17 20:07:53 +0000

    some addons for aat
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@347 0623352e-500e-0410-8e18-d950026d1da6

--- vendor/plugins/acts_as_taggable_on_steroids/lib/tag.rb
+++ vendor/plugins/acts_as_taggable_on_steroids/lib/tag.rb
@@ -7,6 +7,20 @@ class Tag < ActiveRecord::Base
   class << self
     delegate :delimiter, :delimiter=, :to => TagList
   end
+
+  def self.parse(list)
+    tags = []
+    
+    return tags if list.blank?
+    list = list.dup
+    
+    # Parse the quoted tags
+    list.gsub!(/"(.*?)"\s*#{delimiter}?\s*/) { tags << $1; "" }
+    
+    # Strip whitespace and remove blank tags
+    (tags + list.split(delimiter)).map!(&:strip).delete_if(&:blank?)
+  end
+
   
   def ==(object)
     super || (object.is_a?(Tag) && name == object.name)
@@ -18,5 +32,18 @@ class Tag < ActiveRecord::Base
   
   def count
     read_attribute(:count).to_i
+  end
+
+    # Find the tags that are on the same object(s) like this tag
+  def related_tags
+    self.class.find(:all,
+      :select => 'DISTINCT tags.id, tags.name',
+      :joins => 
+          'LEFT OUTER JOIN taggings ON tags.id = taggings.tag_id ' +
+          'LEFT OUTER JOIN taggings ts ON taggings.taggable_id = ts.taggable_id ' +
+          'LEFT OUTER JOIN tags this on ts.tag_id = this.id',
+      :conditions => ['tags.id != ? AND this.id = ? AND taggings.taggable_type = ts.taggable_type', self.id, self.id ],
+      :order => 'tags.name'
+    )
   end
 end
