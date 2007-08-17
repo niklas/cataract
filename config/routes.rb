ad5d5ea522ea45f5a2ed3190199a8b9f397ecbbf
Niklas Hofer <niklas+dev@lanpartei.de>
2007-08-17 16:24:50 +0000

    installed css dryer
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@336 0623352e-500e-0410-8e18-d950026d1da6

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
+  map.connect 'stylesheets/:action.:format', :controller => 'stylesheets'
   map.connect 'torrents/list/:status', :controller => 'torrents', :action => 'list', :status => 'running'
   map.connect '', :controller => "torrents", :action => 'list'
 
