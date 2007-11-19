c28342a2899d2f22ec10e48912b66cefd710ba65
Niklas Hofer <niklas+dev@lanpartei.de>
2007-11-19 22:12:04 +0000

    cap build_rtorrent :)
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@458 0623352e-500e-0410-8e18-d950026d1da6

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,42 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "cataract"
+set :repository,  "svn://lanpartei.de/cataract/trunk"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/usr/lib/cgi-bin/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+role :app, "pomorya.local"
+role :web, "pomorya.local"
+role :download, "pomorya.local"
+role :db,  "pomorya.local", :primary => true
+set :user, 'niklas'
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :build_dir, '/tmp/rtorrent'
+set :libtorrent_version, '0.11.6'
+set :rtorrent_version, '0.7.6'
+
+task :build_rtorrent, :roles => :download do
+  sudo 'aptitude install -q -y g++ checkinstall libsigc++-2.0-dev libxmlrpc-c-dev ncurses-dev libcurl4-openssl-dev'
+  sudo 'dpkg -r rtorrent libtorrent'
+
+  sudo "rm -rf #{build_dir}"
+  run "mkdir -p #{build_dir}"
+
+  run "svn export svn://rakshasa.no/libtorrent/tags/libtorrent-#{libtorrent_version} #{build_dir}/libtorrent"
+  run <<-CMD
+    cd #{build_dir}/libtorrent; 
+    ./autogen.sh && ./configure && make &&
+    sudo checkinstall -y 
+      --pkgversion #{libtorrent_version}cataract
+  CMD
+
+  run "svn export svn://rakshasa.no/libtorrent/tags/rtorrent-#{rtorrent_version} #{build_dir}/rtorrent"
+  run <<-CMD
+    cd #{build_dir}/rtorrent;
+    ./configure 
+      --with-xmlrpc-c && 
+    make && 
+    sudo checkinstall -y
+      --pkgversion #{rtorrent_version}niklas
+  CMD
+
+  sudo "rm -rf #{build_dir}"
+end
