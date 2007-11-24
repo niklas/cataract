915444a14bfa5f393f2b23b9fdff495e7596321d
Niklas Hofer <niklas+dev@lanpartei.de>
2007-11-24 23:15:25 +0000

    use id of watching..
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@470 0623352e-500e-0410-8e18-d950026d1da6

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,5 +1,5 @@
 class WatchesController < ApplicationController
-  before_filter :find_torrent_by_id, :only => [:create, :destroy]
+  # TODO make resourceful
   before_filter :create_log
   helper :torrents
 
@@ -7,21 +7,27 @@ class WatchesController < ApplicationController
     respond_to do |wants|
       wants.js  { render :partial => 'watchlist' }
       wants.xml { render :layout => false }
+      wants.html { render :action => 'list', :controller => 'torrents' }
     end
   end
 
   def create
+    @torrent = Torrent.find params[:torrent_id]
     if current_user.watch(@torrent)
       render_info("Added '#{@torrent.short_title}' to watchlist")
+      render_details_for @torrent
     else
       render_warning("Already watching '#{@torrent.short_title}'")
     end
-    render_details_for @torrent
   end
   def destroy
-    current_user.unwatch(@torrent.id)
-    render_info("Removed '#{@torrent.short_title}' from watchlist")
-    render_details_for @torrent
+    @watching = Watching.find params[:id]
+    if @watching.destroy
+      render_info("Removed '#{@torrent.short_title}' from watchlist")
+      render_details_for @torrent
+    else
+      render_error("Could not unwatch - hrm...")
+    end
   end
 
 end
