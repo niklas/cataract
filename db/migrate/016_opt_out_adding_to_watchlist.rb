8edc6ae2a8f90917ef837b8e31f3903dca853d63
Niklas Hofer <niklas+dev@lanpartei.de>
2007-11-24 18:42:16 +0000

    removed depricated find_all
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@460 0623352e-500e-0410-8e18-d950026d1da6

--- db/migrate/016_opt_out_adding_to_watchlist.rb
+++ db/migrate/016_opt_out_adding_to_watchlist.rb
@@ -1,7 +1,7 @@
 class OptOutAddingToWatchlist < ActiveRecord::Migration
   def self.up
     add_column :users, :dont_watch_new_torrents, :boolean
-    User.find_all.each { |u| u.update_attribute :dont_watch_new_torrents, false}
+    User.find(:all).each { |u| u.update_attribute :dont_watch_new_torrents, false}
   end
 
   def self.down
