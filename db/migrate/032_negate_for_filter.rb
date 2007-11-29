a55090db57f161d96429530e1df62445ca3fcba1
Niklas Hofer <niklas+dev@lanpartei.de>
2007-11-29 00:38:30 +0000

    [changed migration] re-create filters
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@487 0623352e-500e-0410-8e18-d950026d1da6

--- db/migrate/032_negate_for_filter.rb
+++ db/migrate/032_negate_for_filter.rb
@@ -1,9 +1,14 @@
 class NegateForFilter < ActiveRecord::Migration
   def self.up
-    add_column :filters, :negated, :boolean
+    # whoops, dropped filters, so re-create it. (damned)
+    create_table 'filters' do |t|
+      t.column :expression, :string
+      t.column :feed_id, :integer
+      t.column :negated, :boolean
+    end
   end
 
   def self.down
-    remove_column :filters, :negated
+    drop_table :filters
   end
 end
