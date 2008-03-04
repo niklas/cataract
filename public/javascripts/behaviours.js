1edf8cb7b8196eab41917fb1dd07d8832cb816e8
Niklas Hofer <niklas+dev@lanpartei.de>
2008-03-04 00:03:53 +0000

    mark helm busy, the ugly way
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@557 0623352e-500e-0410-8e18-d950026d1da6

--- public/javascripts/behaviours.js
+++ public/javascripts/behaviours.js
@@ -1,3 +1,10 @@
 Event.addBehavior({
-    'div.torrent > a' : Remote.Link
+    'div.torrent > a' : Remote.Link({
+         onCreate: function() { 
+           $('helm').addClassName('busy');
+         }, 
+         onComplete: function() { 
+           $('helm').removeClassName('busy');
+         } 
+     })
 });
