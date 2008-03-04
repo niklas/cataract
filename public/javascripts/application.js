88d7a1d24d9d8d71c0c3958207dc0dfd094b8f73
Niklas Hofer <niklas+dev@lanpartei.de>
2008-03-04 00:53:03 +0000

    remove unused stuff
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@560 0623352e-500e-0410-8e18-d950026d1da6

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,3 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-//Ajax.Responders.register({ 
-// onCreate: function() { 
-//   if (Ajax.activeRequestCount > 0) {
-//     $$('div.loading').each( function(loading_div) {
-//       loading_div.show();
-//     })
-//   }
-// }, 
-// onComplete: function() { 
-//   if (Ajax.activeRequestCount == 0) {
-//     $$('div.loading').each( function(loading_div) {
-//       loading_div.hide();
-//     })
-//   }
-// } 
-//});
-
