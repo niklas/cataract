87b91724aca7d9e18186c2a19c2d3e73f6704fd3
Niklas Hofer <niklas+dev@lanpartei.de>
2008-03-04 00:17:32 +0000

    TODO
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@559 0623352e-500e-0410-8e18-d950026d1da6

--- public/javascripts/lcars.js
+++ public/javascripts/lcars.js
@@ -1,3 +1,7 @@
+// TODO
+//  * count Requests Count of $worker
+//  * only unbusy if == 0
+
 Remote.LinkWithBusy = Behavior.create({
     initialize : function(worker) {
        return new Remote.Link(this.element, {
