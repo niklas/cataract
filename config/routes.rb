28ad24c146e5e9173b12677f13c1739273317680
Niklas Hofer <niklas+dev@lanpartei.de>
2008-03-05 23:16:42 +0000

    nice urls for routes
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@576 0623352e-500e-0410-8e18-d950026d1da6

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,16 @@ ActionController::Routing::Routes.draw do |map|
 
 
 
+
   # Add your own custom routes here.
   # The priority is based upon order of creation: first created -> highest priority.
   
   # Here's a sample route:
   # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
+  map.decoration 'decoration/:kind/:variant/:background_color', 
+    :controller => 'lcars', :action => 'decoration',
+    :defaults => { :background_color => 'black', :variant => 'se', :kind => 'bow' }
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
