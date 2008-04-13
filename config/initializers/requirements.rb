c7342e03bdc012683c2082343ad5ed0f782c0473
Niklas Hofer <niklas+dev@lanpartei.de>
2008-04-13 21:55:16 +0000

    to rails 2.0.2
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@602 0623352e-500e-0410-8e18-d950026d1da6

---
+++ config/initializers/requirements.rb
@@ -0,0 +1,4 @@
+require 'rtorrent'
+gem 'has_finder'
+require 'has_finder'
+
---
+++ config/initializers/settings.rb
@@ -0,0 +1,12 @@
+module SettingsDefaults
+  DEFAULTS = {
+    :history_dir => '[must be configured]',
+    :torrent_dir => '[must be configured]',
+    :target_dir  => '[must be configured]',
+    :min_port    => 6881,
+    :max_port    => 6889,
+    :interval    => 42,
+    :max_up_rate => 5
+  }
+end
+
