833440be9ff207bef9dc3a3b6b0161254d91d428
Niklas Hofer <niklas+dev@lanpartei.de>
2008-04-27 21:35:55 +0000

    rsync spec with cat
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@629 0623352e-500e-0410-8e18-d950026d1da6

--- lib/rsync/rsync_spec.rb
+++ lib/rsync/rsync_spec.rb
@@ -30,6 +30,28 @@ describe "Syncing lots of small files" do
   end
 end
 
+describe "Syncing lots of small files (with cat)" do
+  before(:each) do
+    @stdin, @log, @stderr = Open3.popen3('cat rsync-foo.log')
+    @total = 6734
+    @max_step = 101.0/@total
+    @steps = 0
+  end
+  it "should parse the log" do
+    old_progress = 0.0
+    Rsync.parse(@log) do |progress,current_file|
+      @steps += 1
+      (progress-old_progress).should >= -0.00001 # damned floats
+      (progress-old_progress).should <= @max_step
+      old_progress = progress
+    end
+    @steps.should >= 100
+  end
+  after(:each) do
+    @log.close
+  end
+end
+
 describe "Syncing some epsisodes (unfinished)" do
   before(:each) do
     @log = File.open('rsync-niptuck-unfinished.log')
