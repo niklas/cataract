3fd0b29a0078b1a8f5ce0f014bc9808d6bd0a264
Niklas Hofer <niklas+dev@lanpartei.de>
2008-07-02 22:00:31 +0000

    add decorations only if there did not exist any
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@655 0623352e-500e-0410-8e18-d950026d1da6

--- public/javascripts/lcars.js
+++ public/javascripts/lcars.js
@@ -57,66 +57,69 @@ LcarsBox = Behavior.create({
     return(this.element.className.match(/\b[nwse]{2,3}\b/).first());
   },
   _addDecorations: function() {
-    this.element.getElementsBySelector('img.decoration').each(function(img) {img.remove()});
-    var kind = this._getKind();
-    switch(kind) {
-      case 'nes':
-        this._addDecoration('corner','ne');
-        this._addDecoration('bow','ne');
-        this._addDecoration('corner','se');
-        this._addDecoration('bow','se');
-        this._addDecoration('stump','w','nw');
-        this._addDecoration('stump','w','sw');
-        break;
-      case 'nws':
-        this._addDecoration('corner','nw');
-        this._addDecoration('bow','nw');
-        this._addDecoration('corner','sw');
-        this._addDecoration('bow','sw');
-        this._addDecoration('stump','e','ne');
-        this._addDecoration('stump','e','se');
-        break;
-      case 'ne':
-        this._addDecoration('corner','ne');
-        this._addDecoration('bow','ne');
-        this._addDecoration('stump','w','nw');
-        this._addDecoration('stump','s','se');
-        break;
-      case 'nw':
-        this._addDecoration('corner','nw');
-        this._addDecoration('bow','nw');
-        this._addDecoration('stump','e','ne');
-        this._addDecoration('stump','s','sw');
-        break;
-      case 'sen':
-        this._addDecoration('corner','ne');
-        this._addDecoration('bow','ne');
-        this._addDecoration('corner','se');
-        this._addDecoration('bow','se');
-        this._addDecoration('stump','w','nw');
-        this._addDecoration('stump','w','sw');
-        break;
-      case 'swn':
-        this._addDecoration('corner','nw');
-        this._addDecoration('bow','nw');
-        this._addDecoration('corner','sw');
-        this._addDecoration('bow','sw');
-        this._addDecoration('stump','e','ne');
-        this._addDecoration('stump','e','se');
-        break;
-      case 'se':
-        this._addDecoration('corner','se');
-        this._addDecoration('bow','se');
-        this._addDecoration('stump','w','sw');
-        this._addDecoration('stump','n','ne');
-        break;
-      case 'sw':
-        this._addDecoration('corner','sw');
-        this._addDecoration('bow','sw');
-        this._addDecoration('stump','e','se');
-        this._addDecoration('stump','n','nw');
-        break;
-    };
+    var oldDecors = this.element.getElementsBySelector('img.decoration');
+    if (oldDecors.length < 4) {
+      var kind = this._getKind();
+      switch(kind) {
+        case 'nes':
+          this._addDecoration('corner','ne');
+          this._addDecoration('bow','ne');
+          this._addDecoration('corner','se');
+          this._addDecoration('bow','se');
+          this._addDecoration('stump','w','nw');
+          this._addDecoration('stump','w','sw');
+          break;
+        case 'nws':
+          this._addDecoration('corner','nw');
+          this._addDecoration('bow','nw');
+          this._addDecoration('corner','sw');
+          this._addDecoration('bow','sw');
+          this._addDecoration('stump','e','ne');
+          this._addDecoration('stump','e','se');
+          break;
+        case 'ne':
+          this._addDecoration('corner','ne');
+          this._addDecoration('bow','ne');
+          this._addDecoration('stump','w','nw');
+          this._addDecoration('stump','s','se');
+          break;
+        case 'nw':
+          this._addDecoration('corner','nw');
+          this._addDecoration('bow','nw');
+          this._addDecoration('stump','e','ne');
+          this._addDecoration('stump','s','sw');
+          break;
+        case 'sen':
+          this._addDecoration('corner','ne');
+          this._addDecoration('bow','ne');
+          this._addDecoration('corner','se');
+          this._addDecoration('bow','se');
+          this._addDecoration('stump','w','nw');
+          this._addDecoration('stump','w','sw');
+          break;
+        case 'swn':
+          this._addDecoration('corner','nw');
+          this._addDecoration('bow','nw');
+          this._addDecoration('corner','sw');
+          this._addDecoration('bow','sw');
+          this._addDecoration('stump','e','ne');
+          this._addDecoration('stump','e','se');
+          break;
+        case 'se':
+          this._addDecoration('corner','se');
+          this._addDecoration('bow','se');
+          this._addDecoration('stump','w','sw');
+          this._addDecoration('stump','n','ne');
+          break;
+        case 'sw':
+          this._addDecoration('corner','sw');
+          this._addDecoration('bow','sw');
+          this._addDecoration('stump','e','se');
+          this._addDecoration('stump','n','nw');
+          break;
+      };
+      oldDecors.each(function(img) {img.remove()});
+    }
   },
   _addDecoration: function(kind, variant, klass) {
     img = $img({
@@ -137,12 +140,14 @@ LcarsBox = Behavior.create({
   _decideMenuOrientation: function() {
     var kind = this._getKind();
     this.element.getElementsBySelector('ul.buttons').each(function(buttons) {
-        if (kind.match(/w/)) {
-          buttons.addClassName('left');
-          buttons.removeClassName('right');
-        } else {
-          buttons.removeClassName('left');
-          buttons.addClassName('right');
+        buttons.removeClassName('right');
+        buttons.removeClassName('left');
+        if (buttons.childElements().length > 0) {
+          if (kind.match(/w/)) {
+            buttons.addClassName('left');
+          } else {
+            buttons.addClassName('right');
+          }
         }
     });
   },
@@ -169,4 +174,10 @@ Object.extend(Lcars,{
 
 Event.addBehavior({
     'div.lcars' : LcarsBox
+});
+
+Element.addMethods({
+    resetBehavior: function(element) {
+      $(element).$$assigned = null;
+    }
 });
