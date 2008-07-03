bf800fd7c79a150c4ac2ad338eb06628ea643b19
Niklas Hofer <niklas+dev@lanpartei.de>
2008-07-03 19:45:06 +0000

    image for no_progress
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@662 0623352e-500e-0410-8e18-d950026d1da6

--- public/javascripts/behaviours.js
+++ public/javascripts/behaviours.js
@@ -1,6 +1,6 @@
 Event.addBehavior.reassignAfterAjax = true
 Event.addBehavior({
-    'div.torrent a' : Remote.LinkWithBusy('helm'),
+    '.torrent a' : Remote.Link,
     'div#helm > div.top a' : Remote.LinkWithBusy('helm'),
     '#main > div ul.buttons > li > a ' : Remote.LinkWithBusy('main'),
     'div.torrent > form > select#content_path' : SubDirSelector
