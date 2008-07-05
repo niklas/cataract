6d780df531ee757b0845814cf39908c401c43840
Niklas Hofer <niklas+dev@lanpartei.de>
2008-07-05 23:54:37 +0000

    Torrent creation specced.. a bit
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@688 0623352e-500e-0410-8e18-d950026d1da6

--- spec/models/torrent_spec.rb
+++ spec/models/torrent_spec.rb
@@ -1,7 +1,30 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Torrent do
+describe "A blank Torrent" do
   before(:each) do
+    @torrent = Torrent.new(:url => '', :filename => '')
   end
 
+  it "should not be valid" do
+    @torrent.should_not be_valid
+    @torrent.should have_at_least(1).error_on(:info_hash)
+  end
+
+  it "should not be safable" do
+    lambda do
+      @torrent.save
+    end.should_not change(Torrent,:count).by(1)
+  end
+
+  it "should not be creatable" do
+    lambda do
+      Torrent.create(:url => '', :filename => '')
+    end.should_not change(Torrent,:count).by(1)
+  end
+  it "should not fail saving" do
+    lambda do
+      @torrent.save
+    end.should_not raise_error
+  end
 end
+
