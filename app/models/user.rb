bfa846befe3c1237c9bf68004bfceb15140ce1a2
Niklas Hofer <niklas+dev@lanpartei.de>
2008-07-05 23:56:00 +0000

    User can access jabber settings
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@691 0623352e-500e-0410-8e18-d950026d1da6

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :name, :password, :password_confirmation
+  attr_accessible :login, :email, :name, :password, :password_confirmation, :jabber, :notify_via_jabber
 
 
   def notifiable_via_jabber?
