10d933be1362d4d9db7b396749ebf41896d503ff
Niklas Hofer <niklas+dev@lanpartei.de>
2008-07-05 16:48:39 +0000

    proper start/stop/pause behavior
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@677 0623352e-500e-0410-8e18-d950026d1da6

--- app/controllers/torrents_transfer_controller.rb
+++ app/controllers/torrents_transfer_controller.rb
@@ -3,33 +3,45 @@ class TorrentsTransferController < ApplicationController
   # start
   def create
     @torrent.start!
-    if @torrent.errors.empty?
-      render_notice @torrent.short_title + " has been started for transfer"
-      render :template => '/torrents/show'
-    else
-      render_error "Error while starting: #{@torrent.errors.full_messages.join(',')}"
+    respond_to do |wants|
+      wants.js do
+        if @torrent.errors.empty?
+          render_notice @torrent.short_title + " has been started for transfer"
+          render :template => '/torrents/show'
+        else
+          render_error "Error while starting: #{@torrent.errors.full_messages.join(',')}"
+        end
+      end
     end
   end
 
   # stop
-  def delete
+  def destroy
     @torrent.stop!
-    if @torrent.errors.empty?
-      render_notice @torrent.short_title + " was moved to history"
-      render :template => '/torrents/show'
-    else
-      render_error "Error while stopping: #{@torrent.errors.full_messages.join(',')}"
+    respond_to do |wants|
+      wants.js do
+        if @torrent.errors.empty?
+          render_notice @torrent.short_title + " was moved to history"
+          render :template => '/torrents/show'
+        else
+          render_error "Error while stopping: #{@torrent.errors.full_messages.join(',')}"
+        end
+      end
     end
   end
 
   # pause
   def pause
     @torrent.pause!
-    if @torrent.errors.empty?
-      render_notice @torrent.short_title + " has been paused"
-      render :template => '/torrents/show'
-    else
-      render_error "Error while pausing: #{@torrent.errors.full_messages.join(',')}"
+    respond_to do |wants|
+      wants.js do
+        if @torrent.errors.empty?
+          render_notice @torrent.short_title + " has been paused"
+          render :template => '/torrents/show'
+        else
+          render_error "Error while pausing: #{@torrent.errors.full_messages.join(',')}"
+        end
+      end
     end
   end
 
