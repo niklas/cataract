99f5f9be7e1869bc7f3a474d4ab54fd8f34010ac
Niklas Hofer <niklas+dev@lanpartei.de>
2008-10-30 14:27:18 +0000

    show helper from tirade
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@753 0623352e-500e-0410-8e18-d950026d1da6

--- app/helpers/lcars_helper.rb
+++ app/helpers/lcars_helper.rb
@@ -4,4 +4,31 @@ module LcarsHelper
     col = opts[:color]
     %Q{background: #{col} url(#{lcars_decoration_url opts}) top right no-repeat;}
   end
+
+
+  def show(obj,name,opts={}, &block)
+    label = opts[:label] || _(name.to_s.humanize)
+    if block_given? || !obj.respond_to?(name)
+      concat di_dt_dd(label, capture(&block), :class => name), block.binding
+    else
+      val = obj.send(name) rescue "unknow attr: #{obj.class}##{name}"
+      case val
+      when ActiveRecord::Base
+        val = render(:partial => "/#{val.table_name}/attribute", :object => val)
+      when Array
+        val = list_of(val)
+      when Time
+        val = nice_date(val)
+      end
+      di_dt_dd(label, val, :class => name)
+    end
+  end
+
+  def di_dt_dd(dt,dd, opts={})
+    content_tag(:di,
+                content_tag(:dt, dt) +
+                content_tag(:dd, dd),
+                opts
+               )
+  end
 end
