5f4419e6b04e1f9ec76b018dd1e54d87fbd3fecd
Niklas Hofer <niklas+dev@lanpartei.de>
2008-10-30 14:26:16 +0000

    AR extensions stolen from Tirade
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@750 0623352e-500e-0410-8e18-d950026d1da6

--- vendor/plugins/lcars/init.rb
+++ vendor/plugins/lcars/init.rb
@@ -3,3 +3,8 @@ require File.dirname(__FILE__) + '/lib/lcars'
 Lcars::INCLUDEES.each do |cls|
   cls.send! :include, Lcars
 end
+
+require File.dirname(__FILE__) + '/lib/active_record'
+ActiveRecord::Base.class_eval do
+  include Lcars::ActiveRecord
+end
