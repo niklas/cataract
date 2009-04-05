98b6ce0e5bce1fbc1f4715d9bbc887907e9d9710
Niklas Hofer <niklas+dev@lanpartei.de>
2009-04-05 01:36:04 +0000

    live torrent search with typewatch
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@798 0623352e-500e-0410-8e18-d950026d1da6

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,4 +4,18 @@ $(document).ready(function() {
   $('div.lcars').livequery(function() { $(this).lcars() });
   $('div.lcars a').livequery(function() { $(this).lcarsLink() });
   $('div.lcars form > button ').livequery(function() { $(this).lcarsFormButton() });
+  $('input#term').livequery(function() {
+    var form = $('form#torrent_search');
+    $(this).typeWatch({
+      captureLength : null,
+      callback: function() {
+        $.ajax({
+          type: 'GET',
+          url: form.attr('action'),
+          data: form.serialize(),
+          dataType: "script",
+        });
+      }
+    });
+  });
 });
