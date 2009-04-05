eddfa2f5b87c6896201f1c1677b4b3637b86c80e
Niklas Hofer <niklas+dev@lanpartei.de>
2009-04-05 01:20:46 +0000

    momoize Torrent#series_name
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@794 0623352e-500e-0410-8e18-d950026d1da6

--- app/models/torrent/movie.rb
+++ app/models/torrent/movie.rb
@@ -1,14 +1,15 @@
 class Torrent
+  extend ActiveSupport::Memoizable
   def is_episode?
     !series_name.blank?
   end
 
   def series_name
-    @series ||=
-      Directory.for_series.subdir_names.find do |subdir|
-        toks = subdir.split(/[\._ ]/)
-        self.filename =~ %r[#{toks.join(".*")}]
-      end
+    Directory.for_series.subdir_names.find do |subdir|
+      toks = subdir.split(/[\._ ]/)
+      self.filename =~ %r[#{toks.join(".*")}]
+    end if Directory.for_series
   end
+  memoize :series_name
 end
 
