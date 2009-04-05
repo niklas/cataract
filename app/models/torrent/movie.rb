351f9216e14720e534cb9b5859d12ab434c1d3b4
Niklas Hofer <niklas+dev@lanpartei.de>
2009-04-05 02:58:40 +0000

    split it!
    git-svn-id: svn+ssh://lanpartei.de/home/svn/cataract/trunk@806 0623352e-500e-0410-8e18-d950026d1da6

--- app/models/torrent/movie.rb
+++ app/models/torrent/movie.rb
@@ -6,8 +6,7 @@ class Torrent
 
   def series_name
     Directory.for_series.subdir_names.find do |subdir|
-      toks = subdir.split(/\W+/)
-      self.filename =~ Regexp.new toks.join(".*")
+      self.filename =~ Regexp.new(subdir.split(/\W+/).join(".*"))
     end if Directory.for_series
   end
   memoize :series_name
