16704b380922009cbdd2cd76e819c3fe2361525c
Niklas Hofer <niklas+dev@lanpartei.de>
2011-10-14 22:37:49 +0200

    generated files for 3.0.10

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cataract::Application.config.secret_token = '759018713e4bb859599135795eca45dbdcb23316201973b094ad92c5f07deea444252d136eb67ce2ac660b2b4f4cdae73ce4c60640c681ec16e065c2c31d46f9'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_cataract_session',
-  :secret      => 'f2e32bd1b37a98f1c30ab2dff8fe68c8c414f8c2b0765c9e38f5a0f9c88a4ade0773e61aa1143b7cc2d891080a8e91f5abd26259fbad5ca85e8df77951a35f39'
-}
+Cataract::Application.config.session_store :cookie_store, :key => '_cataract_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Cataract::Application.config.session_store :active_record_store
