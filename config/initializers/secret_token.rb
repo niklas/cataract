81246a370cc471e239279a4185afb57fbd0e2c84
Niklas Hofer <niklas+dev@lanpartei.de>
2011-10-14 22:50:47 +0200

    rake rails:update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cataract::Application.config.secret_token = 'f85e44430c762c9aea53e88ca436cddfc16d0632cc5566bad3b386fce905b6e342f95121310e4cf52c17de22ade6b9df0f38aefb20bf8b1cb85a61489b449c06'
+Cataract::Application.config.secret_token = '38a480b2927cc0d12ff385d402b44d995bd819d07e7e479531581a8a11883a322c7e3e94ac7522efc4bc19e0692df16d276cc43f690868c872225abc42243a3c'
