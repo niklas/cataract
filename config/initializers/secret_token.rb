76c65914440f547c7fe86b6cc56b6f7fa83dd163
Niklas Hofer <niklas+dev@lanpartei.de>
2011-10-14 22:40:42 +0200

    rake rails:update:configs

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cataract::Application.config.secret_token = '759018713e4bb859599135795eca45dbdcb23316201973b094ad92c5f07deea444252d136eb67ce2ac660b2b4f4cdae73ce4c60640c681ec16e065c2c31d46f9'
+Cataract::Application.config.secret_token = 'f85e44430c762c9aea53e88ca436cddfc16d0632cc5566bad3b386fce905b6e342f95121310e4cf52c17de22ade6b9df0f38aefb20bf8b1cb85a61489b449c06'
--- config/routes.rb
+++ config/routes.rb
@@ -1,61 +1,58 @@
-url_base_file = File.join(RAILS_ROOT,'config','urlbase.txt')
-if File.exists? url_base_file
-  url_base = File.read url_base_file
-  url_base = "/#{url_base}" unless url_base =~ /^\//
-  ActionController::AbstractRequest.relative_url_root = url_base
-end
+Cataract::Application.routes.draw do
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
-ActionController::Routing::Routes.draw do |map|
-  map.resources :log_entries
-
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
-
-  map.resources :watchings
-  map.resource :session
-
-  map.resources :statistic, :controller => 'statistic'
-  map.resources :settings
-  map.resources :torrents, 
-    :member => {:fetch => :put},
-    :new => {:probe => :put} do |torrent|
-    torrent.resource :files, :controller => 'torrents_files'
-    torrent.resource :transfer, :controller => 'torrents_transfer', :member => { :pause => :put }
-  end
-
-
-
-
-  # Add your own custom routes here.
-  # The priority is based upon order of creation: first created -> highest priority.
-  
-  # Here's a sample route:
-  # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-  map.decoration 'lcars/decoration/:kind/:variant.png', 
-    :controller => 'lcars', :action => 'decoration', :background_color => 'black', 
-    :requirements => {:kind => /bow|stump|corner/, :variant => /[nsew]{1,2}/}
 
-  map.sparkline '/sparklines/:type/:diameter/:background_color/:remain_color/:share_color/:results.png',
-    :controller =>  'sparklines', :action => 'index'
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
 
-  # You can have the root of your site routed by hooking up '' 
-  # -- just remember to delete public/index.html.
-  #map.login 'login', :controller => 'account', :action => 'login'
-  map.connect 'stylesheets/:action.:format', :controller => 'stylesheets'
-  map.connect 'torrents/list/:status', :controller => 'torrents', :action => 'list', :status => 'running'
-  map.connect '', :controller => "torrents", :action => 'index'
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
 
-  #Hobo.add_routes(map)
-  map.connect 'javascripts/:action.:format', :controller => 'javascripts'
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
 
-  # Allow downloading Web Service WSDL as a file with an extension
-  # instead of a file named 'wsdl'
-  map.connect ':controller/service.wsdl', :action => 'wsdl'
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
 
-  # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id'
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id(.:format)))'
 end
