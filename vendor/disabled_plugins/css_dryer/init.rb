bf942a4b396a41cf15a46ab401df7a79a5d5f574
Niklas Hofer <niklas+dev@lanpartei.de>
2011-10-15 04:05:36 +0200

    remove plugins

--- vendor/disabled_plugins/css_dryer/init.rb
+++
@@ -1,5 +0,0 @@
-require 'css_dryer/processor'
-require 'css_dryer/ncss_handler'
-
-# Register our template handler with Rails.
-ActionView::Template.register_template_handler :ncss, CssDryer::NcssHandler
