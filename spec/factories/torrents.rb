0010fc0ad7f4de017822ad26da2d2500c23b744f
Niklas Hofer <niklas+dev@lanpartei.de>
2011-10-16 03:51:13 +0200

    default torrent from factory is remote and has info_hash

--- spec/factories/torrents.rb
+++ spec/factories/torrents.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :torrent do
     sequence(:title)    { |i| "Torrent ##{i}" }
     sequence(:filename) { |i| "#{i}.torrent" }
+    status 'remote'
+    sequence(:info_hash) { |i| "%0.40d" % i }
   end
 end
