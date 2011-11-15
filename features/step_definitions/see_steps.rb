e5acacbd2ac52b58b2c35a0281f1ab471966aeb3
Niklas Hofer <niklas+dev@lanpartei.de>
2011-11-15 23:13:42 +0100

    make sure list of torrents is present before checking its contents

--- features/step_definitions/see_steps.rb
+++ features/step_definitions/see_steps.rb
@@ -1,6 +1,6 @@
 Then /^I should see a list of the following (.+):$/ do |plural, expected|
   fields = expected.column_names.map(&:underscore).map {|f| ".#{f}" }
-  found = page.all("ul.#{plural} li").map do |item|
+  found = page.find("ul.#{plural}").all("li").map do |item|
     fields.map {|f| item.find(f).text }
   end
   expected.diff! found.unshift(expected.column_names)
