c7a42fa19de8d33d3e10026e4b867d1003555c0e
Niklas Hofer <niklas+dev@lanpartei.de>
2011-11-20 18:43:57 +0100

    ta is an alias to translate_action, action can be symbol

--- app/helpers/action_helper.rb
+++ app/helpers/action_helper.rb
@@ -1,11 +1,15 @@
 module ActionHelper
   def translate_action(key,opts={})
-    if key.present? && key.first == '.'
-      t('helpers.actions' + key, opts)
+    if key.is_a?(Symbol)
+      t("helpers.actions.#{key}", opts)
+    elsif key.present? && key.first == '.'
+      t("helpers.actions#{key}", opts)
     else
       key
     end
   end
+  alias ta translate_action
+
   def link_to(text, *args, &block)
     super translate_action(text), *args, &block
   end
