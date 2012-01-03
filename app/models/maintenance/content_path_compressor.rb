b730781571437ff4cb448abc3793b0b783c57696
Niklas Hofer <niklas+dev@lanpartei.de>
2012-01-03 23:25:14 +0100

    repair torrent whose single file is the infix now

--- app/models/maintenance/content_path_compressor.rb
+++ app/models/maintenance/content_path_compressor.rb
@@ -2,7 +2,15 @@ class Maintenance::ContentPathCompressor < Maintenance::Base
 
   def work
     Torrent.where('content_path_infix IS NOT NULL').each do |t|
-      if t.content_filenames.first.start_with?(t.content_path_infix)
+      if t.content_filenames.length == 1 && t.content_filenames.first == t.content_path_infix
+        unless (t.content_directory.path/t.content_path_infix).exist?
+          t.status = 'missing'
+          t.content_path = nil
+          t.content_path_infix = nil
+          t.save!
+        end
+        # nuffin
+      elsif t.content_filenames.first.start_with?(t.content_path_infix)
         path = t.content_directory.path
         unless path.exist?
           logger.info "Torrent(#{t.id})#content_path '#{path}' does not exist, cannot compress"
@@ -21,17 +29,8 @@ class Maintenance::ContentPathCompressor < Maintenance::Base
         t.content_path = nil
         t.content_path_infix = nil
         t.save!
-
-        self.class.log_infix_update_for_xbmc(infix)
       end
     end
-  end
-
-  private
-  def self.log_infix_update_for_xbmc(infix)
-    xbmc.info sanitize_sql_array([
-      "UPDATE path SET strPath = replace(strPath, :double, :infix) WHERE like(:double, strPath);", infix: infix, double: [infix,infix].join('/')
-    ])
   end
 
 end
