2a7d86b5cd3c1b9954ec21a79db4432da253cabc
Niklas Hofer <niklas+dev@lanpartei.de>
2012-01-04 00:23:20 +0100

    detect multi-torrent as missing

--- app/models/maintenance/content_path_compressor.rb
+++ app/models/maintenance/content_path_compressor.rb
@@ -1,36 +1,39 @@
 class Maintenance::ContentPathCompressor < Maintenance::Base
 
   def work
-    Torrent.where('content_path_infix IS NOT NULL').each do |t|
-      if t.content_filenames.length == 1 && t.content_filenames.first == t.content_path_infix
-        unless (t.content_directory.path/t.content_path_infix).exist?
-          t.status = 'missing'
+    Torrent.where('content_path_infix IS NOT NULL').where('(status IS NULL OR status != ?)', 'missing').each do |t|
+      infix = t.content_path_infix
+      if t.content_filenames.first.start_with?(infix)
+        path = t.content_directory.path
+        full = path/infix
+        unless full.exist?
+          missing! t
+          next
+        end
+        tmp = path/"torrent_#{t.id}_content"
+
+        if full.directory?
+          File.rename full, tmp
+          tmp.children.each do |content|
+            FileUtils.mv content, path
+          end
+          Dir.rmdir tmp
           t.content_path = nil
           t.content_path_infix = nil
           t.save!
         end
-        # nuffin
-      elsif t.content_filenames.first.start_with?(t.content_path_infix)
-        path = t.content_directory.path
-        unless path.exist?
-          logger.info "Torrent(#{t.id})#content_path '#{path}' does not exist, cannot compress"
-          next
-        end
-        tmp = path/"torrent_#{t.id}_content"
-        infix = t.content_path_infix
 
-        File.rename path/infix, tmp
-        tmp.children.each do |content|
-          FileUtils.mv content, path
-        end
-
-        Dir.rmdir tmp
-
-        t.content_path = nil
-        t.content_path_infix = nil
-        t.save!
       end
     end
+  end
+
+  private
+  def missing!(torrent)
+    logger.info "Torrent(#{torrent.id})#content_path '#{torrent.content_path}' does not exist, cannot compress"
+    torrent.status = 'missing'
+    torrent.content_path = nil
+    torrent.content_path_infix = nil
+    torrent.save!
   end
 
 end
