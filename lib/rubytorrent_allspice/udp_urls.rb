47f204dee4e8273075568e9438aaf22303f14752
Niklas Hofer <niklas+dev@lanpartei.de>
2012-01-08 22:38:00 +0100

    allow generic URIs in announce field, too

--- lib/rubytorrent_allspice/udp_urls.rb
+++ lib/rubytorrent_allspice/udp_urls.rb
@@ -1,6 +1,13 @@
 # rubytorrent barks if the annoncelist contains only udp URLs (which are
 # instances of URI::Generic), so we just skip the check alltogether
 class RubyTorrent::MetaInfo
+
+  def initialize(*)
+    super
+    @s.instance_variable_get('@field')[:announce] = URI::Generic
+  end
+
   def check
   end
+
 end
