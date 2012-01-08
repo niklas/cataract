38ca56ae187d108634375ed0e9630adce5fa9922
Niklas Hofer <niklas+dev@lanpartei.de>
2012-01-08 22:48:20 +0100

    doh!

--- lib/rubytorrent_allspice/udp_urls.rb
+++ lib/rubytorrent_allspice/udp_urls.rb
@@ -2,10 +2,12 @@
 # instances of URI::Generic), so we just skip the check alltogether
 class RubyTorrent::MetaInfo
 
-  def initialize(*)
-    super
+  def initialize_with_generic_url(*a)
+    initialize_without_generic_url(*a)
     @s.instance_variable_get('@field')[:announce] = URI::Generic
   end
+
+  alias_method_chain :initialize, :generic_url
 
   def check
   end
