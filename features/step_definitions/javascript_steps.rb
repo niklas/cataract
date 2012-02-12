c082f499ead4a3807d75167420415320f34eba87
Niklas Hofer <niklas+dev@lanpartei.de>
2012-02-13 00:09:31 +0100

    progress pie updates itself every 23s

--- features/step_definitions/javascript_steps.rb
+++ features/step_definitions/javascript_steps.rb
@@ -8,3 +8,10 @@ end
 When /^I wait for the spinner to stop$/ do
   page.should have_no_css('span.ui-icon-loading', visible: true)
 end
+
+When /^the tick interval is reached$/ do
+  page.execute_script <<-EOJS
+    $('body').trigger('tick')
+  EOJS
+  step %Q~I wait for the spinner to stop~
+end
