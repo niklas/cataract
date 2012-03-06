e92857d60e9518d86e23f64ead4abc2acdb3b199
Niklas Hofer <niklas+dev@lanpartei.de>
2012-03-06 22:08:48 +0100

    warmup application after restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,4 +62,9 @@ namespace :deploy do
 
   after "deploy:update_code", "deploy:foreman"
 
+  task :warmup, :roles => :app do
+    STDERR.puts "Warming up application"
+    run "sleep 2; wget -q -O - http://cataract | grep -vi exception > /dev/null"
+  end
+  after 'deploy:restart', 'deploy:warmup'
 end
