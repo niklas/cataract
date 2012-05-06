ee18a3ddc8a3f1d341462e9c1f108d60203f17d0
Niklas Hofer <niklas+dev@lanpartei.de>
2012-05-06 22:45:16 +0200

    cap rake:invoke task=??

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,3 +66,11 @@ namespace :deploy do
   end
   after 'deploy:restart', 'deploy:warmup'
 end
+
+namespace :rake do  
+  desc "Run a task on a remote server."  
+  # run like: cap staging rake:invoke task=a_certain_task  
+  task :invoke do  
+    run("cd #{deploy_to}/current; /usr/bin/env rake --trace #{ENV['task']} RAILS_ENV=#{rails_env}")  
+  end  
+end
