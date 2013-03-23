d7db690c82f52149510b2ec500a051485fabd038
Niklas Hofer <niklas+dev@lanpartei.de>
2013-03-23 02:21:54 +0100

    must fetch URL from db to create name of it

--- app/controllers/torrents_controller.rb
+++ app/controllers/torrents_controller.rb
@@ -25,7 +25,7 @@ class TorrentsController < InheritedResources::Base
   end
 
   def fields_for_collection
-    [:id, :title, :info_hash, :filename, :status, :content_directory_id, :file]
+    [:id, :title, :info_hash, :filename, :status, :content_directory_id, :file, :url]
   end
 
   def search_params
