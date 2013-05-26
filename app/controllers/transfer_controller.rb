d4c39a2123ba1584ecb6f6c5df8b90b6b58c53cb
Niklas Hofer <niklas+dev@lanpartei.de>
2013-05-26 19:13:53 +0200

    provide Transfers for single or collection of Torrents

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -28,10 +28,18 @@ class TransferController < TorrentComponentController
   end
 
   def collection
-    @transfers ||= Torrent.running_or_listed(params[:running]).tap do |torrents|
-      authorize! :index, Torrent
-      Torrent.remote.apply torrents, Fields
-      torrents.reject(&:active?).each(&:finally_stop!)
-    end.map(&:transfer)
+    @transfers ||= torrents_for_collection.tap do  |torrents|
+        authorize! :index, Torrent
+        Torrent.remote.apply torrents, Fields
+        torrents.reject(&:active?).each(&:finally_stop!)
+      end.map(&:transfer)
+  end
+
+  def torrents_for_collection
+    if parent?
+      [parent]
+    else
+      Torrent.running_or_listed(params[:running])
+    end
   end
 end
