f0e3af214a09e419ad905b426ca6fc16d23f8e7b
Niklas Hofer <niklas+dev@lanpartei.de>
2013-06-23 22:30:58 +0200

    configure deployment in unicorn container
    * move deploy TARGET into lanpartei.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,64 +1,28 @@
-# RVM bootstrap
-require 'rvm/capistrano'
-set :rvm_ruby_string, '2.0.0-p195@cataract'
-set :rvm_type, :system
-
-# bundler bootstrap
-require 'bundler/capistrano'
-load 'deploy/assets'
-
-set :whenever_command, "bundle exec whenever"
-require "whenever/capistrano"
-
-# server details
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-set :deploy_via, :remote_cache
-set :use_sudo, false
-
-# application settings
-set :application, "cataract"
-set :scm, :git
 set :repository,  "git://github.com/niklas/cataract.git"
-set :local_repository, "."
-set :branch, 'origin/master'
-set :git_enable_submodules, 1
 
-
-single_target = ENV['TARGET'] || "schnurr.local"
-puts "  will use #{single_target} as target" 
-puts "  set env.TARGET to deploy to another machine"
-
-role :app, single_target
-role :web, single_target
-role :download, single_target
-role :db,  single_target, :primary => true
-set :user, "torrent"
-
-set :deploy_to, "/home/#{user}/www/#{application}"
 set :upstart_dir, "/home/#{user}/.init"
 
 namespace :deploy do
-  desc "Restart App (Apache Passanger)"
-  task :restart do
-    run "touch #{current_release}/tmp/restart.txt"
-  end
 
   desc "Symlink shared stuff"
   task :symlink_shared, :roles => :app do
     config_dir = "#{deploy_to}/#{shared_dir}/config"
-    make_link = "ln -sf #{deploy_to}/{#{shared_dir},#{version_dir}/#{release_name}}"
-    run "mkdir -p #{config_dir}"
-    run "#{make_link}/config/database.yml"
-    run "#{make_link}/config/messenger.yml"
-    run "#{make_link}/config/newrelic.yml"
-    run "#{make_link}/tmp/rtorrent.socket"
-    run "mkdir -p #{deploy_to}/#{shared_dir}/public/uploads"
-    run "#{make_link}/public/uploads"
+    make_link = "ln -sf {#{shared_path},#{latest_release}}"
+
+    run <<-CMD
+      mkdir -p #{shared_path}/config &&
+      mkdir -p #{shared_path}/tmp &&
+      mkdir -p #{shared_path}/public/uploads &&
+      #{make_link}/config/messenger.yml &&
+      #{make_link}/config/newrelic.yml &&
+      #{make_link}/tmp/rtorrent.socket &&
+      #{make_link}/public/uploads
+    CMD
   end
 
-  after "deploy:create_symlink", "deploy:symlink_shared"
-  before "deploy:assets:precompile", "deploy:symlink_shared"
+  after "deploy:finalize_update", "deploy:symlink_shared"
+
+
 
   task :foreman do
     run "mkdir -p #{upstart_dir}"
@@ -68,17 +32,15 @@ namespace :deploy do
   after "deploy:update_code", "deploy:foreman"
   after "deploy:foreman", "services:restart"
 
-  task :warmup, :roles => :app do
-    STDERR.puts "Warming up application"
-    run "sleep 2; wget -q -O - http://#{single_target} | grep -vi exception > /dev/null"
-  end
+
 
   desc "Delete the code we use to accelerate testing"
   task :delete_test_code do
     run "rm -f #{current_release}/app/controllers/test_acceleration_controller.rb"
   end
 
-  before "deploy:assets:precompile", "deploy:delete_test_code"
+  before "deploy:restart", "deploy:delete_test_code"
+
 end
 
 namespace :raketask do
---
+++ config/deploy/lanpartei.rb
@@ -0,0 +1,153 @@
+require 'rvm/capistrano'
+require "bundler/capistrano"
+
+
+set :scm,             :git
+set :local_repository, "."
+set :branch,          "origin/master"
+set :migrate_target,  :current
+
+set :ssh_options,     { :forward_agent => true, :port => '22147' }
+
+set :rails_env,       "production"
+set :deploy_to,       "/home/torrent/app"
+set :normalize_asset_timestamps, false
+
+set :user,            "torrent"
+set :group,           "torrent"
+set :application,     "cataract"
+set :use_sudo,        false
+
+single_target = ENV['TARGET'] || "cataract"
+puts "  will use #{single_target} as target"
+puts "  set env.TARGET to deploy to another machine"
+
+role :app, single_target
+role :web, single_target
+role :db,  single_target, :primary => true
+
+set(:latest_release)  { fetch(:current_path) }
+set(:release_path)    { fetch(:current_path) }
+set(:current_release) { fetch(:current_path) }
+
+set(:current_revision)  { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:latest_revision)   { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip }
+
+default_environment["RAILS_ENV"] = 'production'
+
+# server details
+set :rvm_type, :system
+set(:rvm_ruby_string) { "ruby-2.0.0-p195@#{application}" }
+
+default_run_options[:pty] = true
+
+namespace :deploy do
+  desc "Deploy your application"
+  task :default do
+    update
+    restart
+  end
+
+  desc "Setup your git-based deployment app"
+  task :setup, :except => { :no_release => true } do
+    dirs = [deploy_to, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+    run "git clone #{repository} #{current_path}"
+  end
+
+  task :cold do
+    update
+    migrate
+  end
+
+  task :update do
+    transaction do
+      update_code
+    end
+  end
+
+  desc "Update the deployed code."
+  task :update_code, :except => { :no_release => true } do
+    run "cd #{current_path} && git fetch origin &&  git reset --hard #{branch}"
+    run "cd #{current_path} && git submodule init && git submodule sync && git submodule update"
+    finalize_update
+  end
+
+  desc "Update the database (overwritten to avoid symlink)"
+  task :migrations do
+    transaction do
+      update_code
+    end
+    migrate
+    restart
+  end
+
+  task :finalize_update, :except => { :no_release => true } do
+    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+
+    # mkdir -p is making sure that the directories are there for some SCM's that don't
+    # save empty folders
+    run <<-CMD
+      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+      mkdir -p #{latest_release}/public &&
+      mkdir -p #{latest_release}/tmp &&
+      ln -s #{shared_path}/log #{latest_release}/log &&
+      ln -s #{shared_path}/system #{latest_release}/public/system &&
+      ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
+      ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
+    CMD
+
+    if fetch(:normalize_asset_timestamps, true)
+      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
+      asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
+      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
+    end
+  end
+
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "kill -s USR2 `cat /home/torrent/app/tmp/unicorn.pid`"
+  end
+
+  desc "Start unicorn"
+  task :start, :except => { :no_release => true } do
+    run "cd #{current_path} ; bundle exec unicorn_rails -c /home/torrent/app/shared/unicorn.rb -D"
+  end
+
+  desc "Stop unicorn"
+  task :stop, :except => { :no_release => true } do
+    run "kill -s QUIT `cat /home/torrent/app/tmp/unicorn.pid`"
+  end
+
+  namespace :rollback do
+    desc "Moves the repo back to the previous version of HEAD"
+    task :repo, :except => { :no_release => true } do
+      set :branch, "HEAD@{1}"
+      deploy.default
+    end
+
+    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
+    task :cleanup, :except => { :no_release => true } do
+      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+    end
+
+    desc "Rolls back to the previously deployed version."
+    task :default do
+      rollback.repo
+      rollback.cleanup
+    end
+  end
+end
+
+namespace :lanpartei do
+  desc "updates deploy recipies for lanpartei in #{__FILE__}"
+  task :update do
+    get "#{deploy_to}/shared/deploy.rb", __FILE__
+  end
+end
+
+def run_rake(cmd)
+  run "cd #{current_path}; #{rake} #{cmd}"
+end
