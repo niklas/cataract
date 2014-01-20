8bbf438ebad67f6f046fecf5aa36746376662d0f
Niklas Hofer <niklas+dev@lanpartei.de>
2014-01-20 01:36:50 +0100

    more baba kanuush

--- lib/maulwurf.rb
+++ lib/maulwurf.rb
@@ -6,20 +6,37 @@ class Maulwurf
   end
 
   def self.page direction
-    directives << direction
+    directives << PageDirective.new(direction)
+  end
+
+  def self.file(*a)
+    directives << FileDirective.new(*a)
   end
 
   def process(start_url)
-
   end
 
   class Command
   end
 
   class FollowCommand < Command
-    def initialize(link_name)
-
+    def initialize(link_name, options={})
     end
+  end
+
+  class Directive
+    attr_reader :left
+    attr_reader :right
+    def initialize(directions)
+      @left = directions.keys.first
+      @right = directions.values.first
+    end
+  end
+
+  class PageDirective < Directive
+  end
+
+  class FileDirective < Directive
   end
 
   private
--- lib/torrent_fetcher.rb
+++ lib/torrent_fetcher.rb
@@ -1,4 +1,10 @@
 require 'maulwurf'
 class TorrentFetcher < Maulwurf
   page %r~http://torrentz.eu/\w{40}~ => follow('kickass.to')
+  page %r~http://kickass.to/[^/]+.html~ => follow('#mainDetailsTable .downloadButtonGroup a', title: 'Download torrent file')
+  file 'application/x-bittorrent' => :create_torrent
+
+  def create_torrent(response)
+    Torrent.create payload_data: response.body
+  end
 end
