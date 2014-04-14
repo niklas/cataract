49d332e186d078056b7ac6138baed80fded683fc
Niklas Hofer <niklas+dev@lanpartei.de>
2014-04-14 21:55:59 +0200

    configure cucumber hooks for Filesystem separately

--- spec/support/filesystem.rb
+++ spec/support/filesystem.rb
@@ -112,26 +112,3 @@ RSpec.configure do |config|
   end
 end
 
-if respond_to?(:Before)
-  Before '@rootfs' do
-  end
-
-  After '@rootfs' do
-  end
-
-  After do
-    FileSystem.clear_filesystem!
-  end
-
-  Before '@fakefs' do
-    FileSystem.precache_files!
-    FileSystem.enable_fakefs_on_demand!
-  end
-
-  After '@fakefs' do
-    FileSystem.disable_fakefs_on_demand!
-    FakeFS::FileSystem.clear
-  end
-
-end
-
