45f50358ffb6bfba7ccae2c262f9f47fb425f6ab
Niklas Hofer <niklas+dev@lanpartei.de>
2014-04-19 01:27:07 +0200

    must rule cucumber for zeus

--- config/zeus_plan.rb
+++ config/zeus_plan.rb
@@ -6,6 +6,20 @@ class CustomPlan < Zeus::Rails
     FactoryGirl.reload
     super
   end
+
+  # was removed from zeus default plan in https://github.com/burke/zeus/commit/a3df16778b06ed43af801a6c4c7a2380ce2a844a
+  def cucumber_environment
+    require 'cucumber/rspec/disable_option_parser'
+    require 'cucumber/cli/main'
+    @cucumber_runtime = Cucumber::Runtime.new
+  end
+
+  def cucumber(argv=ARGV)
+    cucumber_main = Cucumber::Cli::Main.new(argv.dup)
+    had_failures = cucumber_main.execute!(@cucumber_runtime)
+    exit_code = had_failures ? 1 : 0
+    exit exit_code
+  end
 end
 
 Zeus.plan = CustomPlan.new
