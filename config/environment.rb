04b990bcd3f5f4b8548803b96675729acaf93c1a
Niklas Hofer <niklas+dev@lanpartei.de>
2014-05-11 22:38:35 +0200

    switch SafeYAML into safe mode

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# as early as possible (database.yml)
+SafeYAML::OPTIONS[:default_mode] = :safe
+
 # Initialize the rails application
 Cataract::Application.initialize!
