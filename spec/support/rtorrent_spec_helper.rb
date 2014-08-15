f1dd359ca6adfa952454805bf0908876b614ed5c
Niklas Hofer <niklas+dev@lanpartei.de>
2014-08-15 22:31:33 +0200

    Revert "let's see if rtorrent is actually started"
    This reverts commit 1bdabd7e897a797b80b4889c036fbab4a3222af9.
    
    it runs - just was the wrong version (too new)

--- spec/support/rtorrent_spec_helper.rb
+++ spec/support/rtorrent_spec_helper.rb
@@ -27,7 +27,6 @@ module RTorrentSpecHelper
       end
       Torrent.reset_remote!
       Torrent.stub(:rtorrent_socket_path).and_return(rtorrent_socket_path)
-      system 'ps af'
     else
       raise "could not start rtorrent"
     end
