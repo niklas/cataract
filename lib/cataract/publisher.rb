72e51e56a9f1dbcb9ce77da5e66eb8a322cde6b4
Niklas Hofer <niklas+dev@lanpartei.de>
2015-04-14 21:16:55 +0200

    provide a way to publish a serialized record

--- lib/cataract/publisher.rb
+++ lib/cataract/publisher.rb
@@ -1,10 +1,16 @@
 require 'redis'
 class Cataract::Publisher
   def self.publish(channel, message)
-    redis.publish "message.#{channel}", message.to_json
+    channel = "message.#{channel}" unless channel.include?('.')
+    redis.publish channel, message.to_json
   rescue Redis::CannotConnectError => e
     # Travis fails to start redis on 2015-04-05
     Rails.logger.warn { e.to_s }
+  end
+
+  def self.publish_record_update(record)
+    serializer = record.active_model_serializer.new(record)
+    publish 'update', serializer.to_json
   end
 
   def self.redis
