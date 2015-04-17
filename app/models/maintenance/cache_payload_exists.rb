2977c84834a574ddf53739ec4dca4517442ccca9
Niklas Hofer <niklas+dev@lanpartei.de>
2015-04-17 17:35:04 +0200

    make payload cache more robust

--- app/models/maintenance/cache_payload_exists.rb
+++ app/models/maintenance/cache_payload_exists.rb
@@ -3,12 +3,17 @@ class Maintenance::CachePayloadExists < Maintenance::Base
     Torrent.transaction do
       Torrent.find_in_batches batch_size: 333 do |batch|
         batch.each do |torrent|
-          really = torrent.payload.exists?
-          cached = torrent.payload_exists?
+          begin
+            really = torrent.payload.exists?
+            cached = torrent.payload_exists?
 
-          if really != cached
-            torrent.payload_exists = really
-            torrent.save!
+            if really != cached
+              torrent.payload_exists = really
+              logger.info { "Torrent #{torrent.id} really #{really ? 'has' : 'has no'} payload" }
+              torrent.save!
+            end
+          rescue StandardError => e
+            logger.warn { "Could not check Torrent #{torrent.id}'s content: #{e}" }
           end
         end
       end
