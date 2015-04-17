8211c2ef256285409b4182f8ee117872beead18e
Niklas Hofer <niklas+dev@lanpartei.de>
2015-04-17 17:27:44 +0200

    rename badly encoded files before importing

--- app/models/maintenance/recognizer.rb
+++ app/models/maintenance/recognizer.rb
@@ -12,6 +12,11 @@ class Maintenance::Recognizer < Maintenance::Base
         logger.info { "sync - in #{directory.full_path}" }
         directory.glob('*.torrent').each do |filepath|
           logger.info { "sync - found: #{filepath}" }
+          unless filepath.valid_encoding?
+            n = filepath.scrub
+            File.rename filepath, n
+            filepath = n
+          end
           torrent = Torrent.new(status: 'new', file: File.open(filepath))
           if torrent.save
             created << torrent 
