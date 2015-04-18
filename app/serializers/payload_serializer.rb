e2abdd5c6f2326de41ff12aadc45e3fcd88c4502
Niklas Hofer <niklas+dev@lanpartei.de>
2015-04-18 19:28:53 +0200

    format size with helper

--- app/serializers/payload_serializer.rb
+++ app/serializers/payload_serializer.rb
@@ -1,11 +1,9 @@
 class PayloadSerializer < BaseSerializer
-  include TorrentsHelper
   attributes :torrent_id, :filenames, :size
 
   def attributes
     super.tap do |hash|
       hash['directory_id'] = object.torrent.content_directory_id
-      hash['human_size'] = human_bytes(object.size).sub(/ytes$/,'')
       hash['id'] = object.torrent_id # trick ember
     end
   end
